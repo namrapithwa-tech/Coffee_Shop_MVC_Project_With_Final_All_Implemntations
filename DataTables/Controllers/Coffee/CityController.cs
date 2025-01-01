f1626d560d26d6b94cd1b3aa3ceba9865d4ee233
namrapithwa-tech <164359724+namrapithwa-tech@users.noreply.github.com>
2025-01-01 08:11:17 +0530

    All CRUD Work
    Dashboard Coming soon...!!!

---
+++ DataTables/Controllers/Coffee/CityController.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+using DataTables.Models.Coffee;
+using DataTables.Models;
+
+namespace DataTables.Controllers.Coffee
+{
+    public class CityController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        #region configuration
+        public CityController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        #endregion
+
+        #region City List
+        public IActionResult CityList()
+        {
+            string connectionstr = this._configuration.GetConnectionString("myConnectionString");
+            //PrePare a connection
+            DataTable dt = new DataTable();
+            SqlConnection conn = new SqlConnection(connectionstr);
+            conn.Open();
+
+            //Prepare a Command
+            SqlCommand objCmd = conn.CreateCommand();
+            objCmd.CommandType = CommandType.StoredProcedure;
+            objCmd.CommandText = "PR_LOC_City_SelectAll";
+
+            SqlDataReader objSDR = objCmd.ExecuteReader();
+            dt.Load(objSDR);
+            conn.Close();
+            return View("CityList", dt);
+        }
+        #endregion
+
+        #region Add/Edit City
+        public IActionResult AddEditCity(int? CityID)
+        {
+            CityModel model = new CityModel();
+            if (CityID != null && CityID > 0) // Edit mode
+            {
+                string connectionstr = this._configuration.GetConnectionString("myConnectionString");
+                using (SqlConnection conn = new SqlConnection(connectionstr))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "PR_LOC_City_SelectByPK";
+                        cmd.Parameters.AddWithValue("@CityID", CityID);
+
+                        using (SqlDataReader dr = cmd.ExecuteReader())
+                        {
+                            if (dr.Read())
+                            {
+                                model.CityID = Convert.ToInt32(dr["CityID"]);
+                                model.CityName = dr["CityName"].ToString();
+                                model.CityCode = dr["CityCode"].ToString();
+                                model.StateID = Convert.ToInt32(dr["StateID"]);
+                                model.CountryID = Convert.ToInt32(dr["CountryID"]);
+                            }
+                        }
+                    }
+                }
+            }
+            return View("AddEditCity", model);
+        }
+        #endregion
+
+        #region Save City
+        [HttpPost]
+        [HttpPost]
+        public IActionResult SaveCity(CityModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    string connectionstr = this._configuration.GetConnectionString("myConnectionString");
+                    using (SqlConnection conn = new SqlConnection(connectionstr))
+                    {
+                        conn.Open();
+                        using (SqlCommand cmd = conn.CreateCommand())
+                        {
+                            cmd.CommandType = CommandType.StoredProcedure;
+                            if (model.CityID > 0)
+                            {
+                                cmd.CommandText = "PR_LOC_City_Update";
+                                cmd.Parameters.AddWithValue("@CityID", model.CityID);
+                            }
+                            else
+                            {
+                                cmd.CommandText = "PR_LOC_City_Insert";
+                            }
+
+                            cmd.Parameters.AddWithValue("@CityName", model.CityName);
+                            cmd.Parameters.AddWithValue("@CityCode", model.CityCode);
+                            cmd.Parameters.AddWithValue("@StateID", model.StateID);
+                            cmd.Parameters.AddWithValue("@CountryID", model.CountryID);
+
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    TempData["Message"] = model.CityID > 0 ? "City updated successfully." : "City added successfully.";
+                    return RedirectToAction("CityList");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["Message"] = $"An error occurred: {ex.Message}";
+            }
+            return View("AddEditCity", model);
+        }
+
+        #endregion
+
+
+        #region Delete City
+        public IActionResult Delete(int CityID)
+        {
+            string connectionstr = this._configuration.GetConnectionString("myConnectionString");
+            using (SqlConnection conn = new SqlConnection(connectionstr))
+            {
+                conn.Open();
+                using (SqlCommand sqlCommand = conn.CreateCommand())
+                {
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.CommandText = "PR_LOC_City_Delete";
+                    sqlCommand.Parameters.AddWithValue("@CityID", CityID);
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
+            return RedirectToAction("CityList");
+        }
+        #endregion
+    }
+}
---
+++ DataTables/Models/Coffee/CityModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace DataTables.Models.Coffee
+{
+    public class CityModel
+    {
+        public int CityID { get; set; }
+
+        [Required]
+        [DisplayName("City Name")]
+        public string CityName { get; set; }
+
+        [Required]
+        [DisplayName("Country Name")]
+        public int CountryID { get; set; }
+
+        [Required]
+        [DisplayName("State ID")]
+        public int StateID { get; set; }
+
+        [Required]
+        [DisplayName("State Name")]
+        public string StateName { get; set; }
+
+        [Required]
+        [DisplayName("State Name")]
+        public string CountryName { get; set; }
+
+        [DisplayName("City Code")]
+        public string CityCode { get; set; }
+        public int? UserID { get; set; }
+    }
+}
